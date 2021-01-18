2d8088f998e730d8533a910e68d7f84e9388acb4
Stanislav <1ven970@gmail.com>
2021-01-18 14:55:04 +0300

    maybe

--- script/btn.js
+++ script/btn.js
@@ -4,17 +4,22 @@ function(){
 		let rotateElem = document.querySelectorAll('.instructors');
 		let container = document.querySelector('.container-center__instructor');
 		var btnBack = document.querySelectorAll('.btnBack');
-
+		var rearSide = document.querySelectorAll('.rear');
 		container.addEventListener('click', rotate);
 
 	function	rotate(e){
          for(let i=0; i<btn.length; i++){
 				if(e.target==btn[i]){
 					rotateElem[i].classList.add('back-side');
+						rearSide[i].style.backfaceVisibility = 'visible';
 				}
 				if(e.target == btnBack[i]){
-					rotateElem[i].classList.remove('back-side');
+					rotateElem[i].classList.remove('back-side');		
+					rearSide[i].style.backfaceVisibility = 'hidden';
+
 				}
 			}
 		}
+
+		
 })
