316a28d5c6b4d970d167f931f3a522d53e4a0c6a
Stanislav <1ven970@gmail.com>
2021-01-18 15:52:12 +0300

    btn

--- script/btn.js
+++ script/btn.js
@@ -1,21 +1,32 @@
-window.addEventListener('DOMContentLoaded', function() {
-	let btn = document.querySelectorAll('button');
-	let rotateElem = document.querySelectorAll('.instructors');
-	let container = document.querySelector('.container-center__instructor');
-	var btnBack = document.querySelectorAll('.btnBack');
-	var rearSide = document.querySelectorAll('.rear');
-	container.addEventListener('click', rotate);
+window.addEventListener('DOMContentLoaded',
+function(){
+		let btn = document.querySelectorAll('button');
+		let rotateElem = document.querySelectorAll('.instructors');
+		let container = document.querySelector('.container-center__instructor');
+		var btnBack = document.querySelectorAll('.btnBack');
+		var rearSide = document.querySelectorAll('.rear');
+		container.addEventListener('click', rotate);
 
-	function rotate(e) {
-		 for (let i = 0; i < btn.length; i++) {
-			  if (e.target == btn[i]) {
+	function	rotate(e){
+         for(let i=0; i<btn.length; i++){
+				if(e.target==btn[i]){
 					rotateElem[i].classList.add('back-side');
-					rearSide[i].style.backfaceVisibility = 'visible';
-			  }
-			  if (e.target == btnBack[i]) {
-					rotateElem[i].classList.remove('back-side');
+						rearSide[i].style.backfaceVisibility = 'visible';
+				}
+				if(e.target == btnBack[i]){
+					rotateElem[i].classList.remove('back-side');		
 					rearSide[i].style.backfaceVisibility = 'hidden';
-			  }
-		 }
-	}
+
+
+				}
+				if(rotateElem[i].classList.contains('back-side')){
+					btn[i].style.display = 'none';
+				}
+				else{
+					btn[i].style.display = 'block';
+				}
+			}
+		}
+
+		
 })
